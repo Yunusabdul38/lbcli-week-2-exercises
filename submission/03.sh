e29c9f10eec705c6e4d531c3232547342fb05937
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-30 23:29:16 +0100

    1-8

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Create a SegWit address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress  "bech32")  
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 $SEGWIT_ADDRESS > /dev/null 2>&1
 # Return only the Address
+echo $SEGWIT_ADDRESS
